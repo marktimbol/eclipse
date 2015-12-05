ffbe28e1a1dd824e235f0d8c6cf59754083953bf
Mark <mark@Marks-MacBook-Pro.local>
2015-12-05 18:52:36 +0400

    Fixing packages page

--- resources/views/public/packages/index.blade.php
+++ resources/views/public/packages/index.blade.php
@@ -4,6 +4,8 @@
 
 @section('body_class', 'page')
 
+@inject('categories', 'Eclipse\Repositories\Category\CategoryRepositoryInterface')
+
 @section('content')
 	<div class="container">
 		<div class="row">
@@ -11,7 +13,38 @@
 				<h1 class="page__title">Packages</h1>
 
 				<div class="page__description">
-					@include('public.partials._packages')
+					<div class="row">
+						<div class="col s12 m3">
+							<div class="filter-package">
+								<h5 class="filter-package__title">Filter</h5>
+
+								@foreach( $categories->all() as $category )
+									<h6 class="filter-package__category">
+										<a href="{{ route('category', $category->slug) }}">
+											{{ $category->name }}
+										</a>
+									</h6>
+
+									@if( count( $category->packages ) )
+										<ul>
+											@foreach( $category->packages as $package )
+												<li>
+													<a href="{{ route('package', $package->slug) }}">
+														{{ $package->name }}
+													</a>
+												</li>
+											@endforeach
+										</ul>
+									@endif
+								@endforeach
+							</div>
+						</div>
+
+						<div class="col s12 m9">
+							@include('public.partials._packages')
+						</div>
+
+					</div>
 				</div>
 			</div>
 		</div>
