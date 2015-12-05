aae6abcff15b863cf084063170fa91416f909d05
Mark <mark@Marks-MacBook-Pro.local>
2015-12-05 19:01:46 +0400

    Updated December 5, 2015

--- resources/views/public/packages/index.blade.php
+++ resources/views/public/packages/index.blade.php
@@ -15,33 +15,37 @@
 				<div class="page__description">
 					<div class="row">
 						<div class="col s12 m3">
-							<div class="filter-package">
-								<h5 class="filter-package__title">Filter</h5>
+							<div class="card-panel">
+								<div class="filter-package">
+									<h5 class="filter-package__title">Filter</h5>
 
-								@foreach( $categories->all() as $category )
-									<h6 class="filter-package__category">
-										<a href="{{ route('category', $category->slug) }}">
-											{{ $category->name }}
-										</a>
-									</h6>
+									@foreach( $categories->all() as $category )
+										<h6 class="filter-package__category">
+											<a href="{{ route('category', $category->slug) }}">
+												{{ $category->name }}
+											</a>
+										</h6>
 
-									@if( count( $category->packages ) )
-										<ul>
-											@foreach( $category->packages as $package )
-												<li>
-													<a href="{{ route('package', $package->slug) }}">
-														{{ $package->name }}
-													</a>
-												</li>
-											@endforeach
-										</ul>
-									@endif
-								@endforeach
+										@if( count( $category->packages ) )
+											<ul>
+												@foreach( $category->packages as $package )
+													<li>
+														<a href="{{ route('package', $package->slug) }}">
+															{{ $package->name }}
+														</a>
+													</li>
+												@endforeach
+											</ul>
+										@endif
+									@endforeach
+								</div>
 							</div>
 						</div>
 
 						<div class="col s12 m9">
-							@include('public.partials._packages')
+							<div class="card-panel">
+								@include('public.partials._packages')
+							</div>
 						</div>
 
 					</div>
