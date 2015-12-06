649c63c9c1bd37b0b0ad47aff775bad2660d9350
Mark Timbol <mark.timbol@hotmail.com>
2015-12-06 17:24:14 +0400

    Fix Stripe.js

--- resources/assets/js/libs/stripe-billing.js
+++ resources/assets/js/libs/stripe-billing.js
@@ -1,45 +1,50 @@
 (function() {
+
+    $payment = jQuery.noConflict();
+
     var StripeBilling = {
-        init: function() {
-           this.form = $('#billing-form');
+        init: function()
+        {
+           this.form = $payment('#billing-form');
            this.submitButton = this.form.find('button[type=submit]');
            this.submitButtonValue = this.submitButton.val();
            this.form.find('.payment-errors').hide();
 
-           var stripeKey = $('meta[name="publishable-key"]').attr('content');
+           var stripeKey = $payment('meta[name="publishable-key"]').attr('content');
 
            Stripe.setPublishableKey(stripeKey);
 
            this.bindEvents();
         },
 
-        bindEvents: function() {
-            this.form.on('submit', $.proxy(this.sendToken, this));
+        bindEvents: function()
+        {
+            this.form.on('submit', $payment.proxy(this.sendToken, this));
         },
 
-        sendToken: function(event) {
-
+        sendToken: function(event)
+        {
             this.submitButton.hide();
 
             this.form.find('.progress').show();
 
-            Stripe.createToken(this.form, $.proxy(this.stripeResponseHandler, this));
+            Stripe.createToken(this.form, $payment.proxy(this.stripeResponseHandler, this));
 
             event.preventDefault();
         },
 
-        stripeResponseHandler: function(status, response) {
-            if (response.error) {
-                
+        stripeResponseHandler: function(status, response)
+        {
+            if (response.error)
+            {    
                 this.form.find('.payment-errors').show().text(response.error.message);
 
                 this.form.find('.progress').hide();
 
                 return this.submitButton.show();
-            
             }
 
-            $('<input>', {
+            $payment('<input>',{
                 type: 'hidden',
                 name: 'token',
                 value: response.id
