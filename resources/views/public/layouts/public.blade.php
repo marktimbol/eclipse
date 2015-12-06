3d41d77eea5a3a37099c7dfef4aa33865c1f935d
Mark Timbol <mark.timbol@hotmail.com>
2015-12-06 14:27:45 +0400

    Fixed https

--- resources/views/public/layouts/public.blade.php
+++ resources/views/public/layouts/public.blade.php
@@ -9,7 +9,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 	<!--Import Google Icon Font-->
-	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="{{ elixir('css/public.css') }}" />
 
 	@yield('header_styles')
